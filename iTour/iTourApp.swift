d33c1a4b0702b4905497432d8e93c1283b577e67
Furqan Ali <furqan.ali9500@gmail.com>
2025-03-17 03:37:43 +0900

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Furqan Ali on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iTourTests/iTourTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTourTests.swift
+//  iTourTests
+//
+//  Created by Furqan Ali on 3/17/25.
+//
+
+import Testing
+@testable import iTour
+
+struct iTourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
